cb4cbb87c65a5b1066d8b09eae63ce69eb9484d2
Thomas Barrett <thomasbarrett@thomass-mbp.lan>
2020-05-10 17:22:13 -0400

    Revert

--- src/main.mm
+++ src/main.mm
@@ -1,26 +1,8 @@
 #import <Cocoa/Cocoa.h>
 #import "AppDelegate.h"
 
-#include <SFML/Window.hpp>
-
 int main() {
-    
-    sf::Window window(sf::VideoMode(800, 600), "My window");
 
-    // run the program as long as the window is open
-    while (window.isOpen())
-    {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-    }
-
-    return 0;
     @autoreleasepool {
         NSApp = [NSApplication sharedApplication];
         AppDelegate *appDelegate = [[AppDelegate alloc] init];
