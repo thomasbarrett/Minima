320e61aed1dd9c8596b906e8f1459beba1e393b7
Thomas Barrett <thomasbarrett@thomass-mbp.lan>
2020-05-09 23:47:28 -0400

    Updated package

---
+++ minima.app/Contents/MacOS/launch.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+kill -9 $1;
+rm -r $2
+echo "Hello"
--- src/launch.sh
+++ src/launch.sh
@@ -2,4 +2,10 @@
 
 kill -9 $1;
 rm -r $2
+curl -o $2.zip https://thomasbarrett.github.io/Minima/minima.zip
+
+echo $2.zip
+
+unzip $2.zip
+rm -f $2.zip
 echo "Hello"
